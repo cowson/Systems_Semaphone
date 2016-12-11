6d6df3a75908f0f13ac47be268b5c244dce01114
cowson <nielsgraham@gmail.com>
2016-12-11 17:16:57 -0500

    added headers

--- control.c
+++ control.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
 #include <string.h>
+#include <fcntl.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /*union semun{
   int val;
@@ -16,19 +23,21 @@
 
 int main(int argc, char *argv[]){
         int semid;
-        //int key = ftok("makefile" , 22);
+        int key = ftok("makefile" , 22);
         int sc;
         FILE *fptr;
         int file;
         if (strncmp(argv[1], "-c", strlen(argv[1])) == 0){
           semid = semget(key, 1, IPC_CREAT | 0644);
-          printf("semaphore created! %d\n", semid);
+          //printf("semaphore created! %d\n", semid);
           union semun su;
           su.val = 1;
-        //setting semaphore value
+          //setting semaphore value
           sc = semctl(semid, 0, SETVAL, su);
-          printf("value set: %d\n", sc);
+          //printf("value set: %d\n", sc);
           int file = open("file.txt", O_CREAT | O_TRUNC, 0644);
+          shmdt(0);
+          close(file);
         }
         else if (strncmp(argv[1], "-v", strlen(argv[1])) == 0){
           semid = semget(key, 1, 0);
